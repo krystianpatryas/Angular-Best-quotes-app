94c94491693bd36c27cfdac12b37b6d0c02ea71e
Krystian <krystianpatryas9@gmail.com>
2022-07-19 00:04:05 +0200

    new features

---
+++ src/app/ranking/ranking.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Quotation } from '../models/quotation';
+
+@Component({
+  selector: 'app-ranking',
+  templateUrl: './ranking.component.html',
+  styleUrls: ['./ranking.component.css']
+})
+export class RankingComponent {
+
+  @Input()
+ quotes: Quotation[];
+}
